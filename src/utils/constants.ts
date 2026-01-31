ed00be7bc974646590cfda12520fae8655da8717
Andrey Kalashnikov <andrey.kalashnikov@muehlemann-popp.ch>
2026-01-31 11:46:39 +0400

    constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const HARDCODED_EXCLUDES = ['node_modules/**', '.git/**', '.venv/**', '__pycache__/**', 'dist/**', 'build/**'];
+
+export const CONFIG_PATH = '.vscode/context-config.json';
+
+export const DEFAULT_DEBOUNCE_MS = 3000;
+export const DEFAULT_MAX_FILE_SIZE_KB = 1024;
+export const DEFAULT_MAX_TOTAL_FILES = 500;
